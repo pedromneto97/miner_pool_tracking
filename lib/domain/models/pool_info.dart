bf3b664596631a980e9a131acaf8c4dc2e67fec2
Pedro Neto <pedromneto97@gmail.com>
2021-12-30 23:37:08 -0300

    feat: add pool info model

---
+++ lib/domain/models/pool_info.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pool_info.g.dart';
+
+@JsonSerializable()
+class PoolInfo extends Equatable {
+  final double minerReward;
+  final double blockTime;
+  final double networkHash;
+
+  const PoolInfo({
+    required this.minerReward,
+    required this.blockTime,
+    required this.networkHash,
+  });
+
+  factory PoolInfo.fromJson(Map<String, dynamic> json) => _$PoolInfoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PoolInfoToJson(this);
+
+  @override
+  List<Object?> get props => [minerReward, blockTime, networkHash];
+}
