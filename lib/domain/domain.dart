076173336b61639943b5fb9f9deb573f998d4677
Pedro Neto <pedromneto97@gmail.com>
2021-12-30 23:39:54 -0300

    feat: add use cases

---
+++ lib/domain/domain.dart
@@ -0,0 +1,4 @@
+export 'models/models.dart';
+export 'repositories/repositories.dart';
+export 'services/services.dart';
+export 'use-cases/use_cases.dart';
---
+++ lib/domain/use-cases/use_cases.dart
@@ -0,0 +1,4 @@
+export 'mining/mining_info.dart';
+export 'wallet/add_wallet.dart';
+export 'wallet/get_wallets.dart';
+export 'wallet/remove_wallet.dart';
