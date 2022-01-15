967c20b2b3c381d552a29b4669c101ad79588ee9
Pedro Neto <pedromneto97@gmail.com>
2022-01-15 19:41:38 -0300

    feat: add datetime converter

---
+++ lib/utils/datetime_converter.dart
@@ -0,0 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+
+class DateTimeEpochConverter implements JsonConverter<DateTime, int> {
+  const DateTimeEpochConverter();
+
+  @override
+  DateTime fromJson(int json) => DateTime.fromMillisecondsSinceEpoch(json);
+
+  @override
+  int toJson(DateTime object) => object.millisecondsSinceEpoch;
+}
