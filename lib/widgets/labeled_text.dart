cc5f8f8b254e6e10928ebe243e4d8fc8e35046ee
Pedro Neto <pedromneto97@gmail.com>
2022-01-15 16:38:47 -0300

    feat: add tooltip to labeled text

--- lib/widgets/labeled_text.dart
+++ lib/widgets/labeled_text.dart
@@ -3,25 +3,49 @@ import 'package:flutter/material.dart';
 class LabeledText extends StatelessWidget {
   final String label;
   final String text;
+  final String tooltipMessage;
+
   const LabeledText({
     Key? key,
     required this.text,
     required this.label,
+    this.tooltipMessage = '',
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        text: label,
-        style: Theme.of(context).textTheme.bodyText1,
-        children: [
-          TextSpan(
-            text: text,
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildLabelWidget(context),
+        Text(
+          text,
+          style: Theme.of(context).textTheme.subtitle2,
+        ),
+      ],
     );
+  }
+
+  Widget _buildLabelWidget(BuildContext context) {
+    final Text text = Text(
+      label,
+      style: Theme.of(context).textTheme.overline,
+    );
+
+    return tooltipMessage.isNotEmpty
+        ? Row(
+            children: [
+              text,
+              Tooltip(
+                message: tooltipMessage,
+                child: const Icon(
+                  Icons.info_outline_rounded,
+                  size: 12,
+                ),
+                triggerMode: TooltipTriggerMode.tap,
+              ),
+            ],
+          )
+        : text;
   }
 }
