b6dc8154dba6ec6cc5248ef5c8f400a6d1dd7c31
Pedro Neto <pedromneto97@gmail.com>
2022-01-15 16:40:26 -0300

    refactor: use consumption from wallet

--- lib/presenter/mining/mining_info.dart
+++ lib/presenter/mining/mining_info.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../utils/string_extension.dart';
@@ -21,18 +20,6 @@ class MiningInfoPage extends StatefulWidget {
 }
 
 class _MiningInfoPageState extends State<MiningInfoPage> {
-  final consumptionController = TextEditingController(text: '');
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -60,49 +47,25 @@ class _MiningInfoPageState extends State<MiningInfoPage> {
           ),
         ),
         body: SingleChildScrollView(
+          clipBehavior: Clip.antiAlias,
+          primary: true,
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: consumptionController,
-                        decoration: InputDecoration(
-                          label: const Text('Consumption'),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          suffixText: 'W',
-                          contentPadding: const EdgeInsets.all(16),
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                        ],
-                        keyboardType: TextInputType.number,
-                      ),
+            child: BlocBuilder<MiningInfoBloc, MiningInfoState>(
+              builder: (context, state) {
+                if (state is MiningInfoSuccess) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: MiningInfoDetails(
+                      miningData: state.miningData,
+                      consumption: widget.wallet.consumption,
                     ),
-                  ],
-                ),
-                BlocBuilder<MiningInfoBloc, MiningInfoState>(
-                  builder: (context, state) {
-                    if (state is MiningInfoSuccess) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: ValueListenableBuilder<TextEditingValue>(
-                          builder: (_, consumption, __) => MiningInfoDetails(
-                            miningData: state.miningData,
-                            consumption: consumption.text.isNotEmpty ? int.parse(consumption.text) : 0,
-                          ),
-                          valueListenable: consumptionController,
-                        ),
-                      );
-                    }
-                    return const CircularProgressIndicator();
-                  },
-                ),
-              ],
+                  );
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
           ),
         ),
